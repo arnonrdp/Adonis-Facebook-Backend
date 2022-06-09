bd4059d06b974d4925d2064479c47900ca8ff0d6
Arnon <arnonrdp@gmail.com>
2022-06-09 08:51:00 -0300

    Aplicando transaction

--- app/Controllers/Http/Users/Register.ts
+++ app/Controllers/Http/Users/Register.ts
@@ -1,31 +1,62 @@
 import { faker } from '@faker-js/faker'
 import Mail from '@ioc:Adonis/Addons/Mail'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { User } from 'App/Models'
-import { StoreValidator } from 'App/Validators/User/Register'
+import Database from '@ioc:Adonis/Lucid/Database'
+import { User, UserKey } from 'App/Models'
+import { StoreValidator, UpdateValidator } from 'App/Validators/User/Register'
 
 export default class UserRegisterController {
-  public async store({ request }: HttpContextContract) {
-    const { email, redirectUrl } = await request.validate(StoreValidator)
-    const user = await User.create({ email })
+  public async store({ request, response }: HttpContextContract) {
+    await Database.transaction(async (trx) => {
+      const { email, redirectUrl } = await request.validate(StoreValidator)
+      const user = new User()
 
-    await user.save()
+      user.useTransaction(trx)
 
-    const key = faker.datatype.uuid() + new Date().getTime()
+      user.email = email
 
-    user.related('keys').create({ key })
+      await user.save()
 
-    const link = `${redirectUrl.replace(/\/$/, '')}/${key}`
+      const key = faker.datatype.uuid() + new Date().getTime()
 
-    await Mail.send((message) => {
-      message.to(email)
-      message.from('contato@facebook.com', 'Facebook')
-      message.subject('Confirmação de cadastro')
-      message.htmlView('emails/register', { link })
+      user.related('keys').create({ key })
+
+      const link = `${redirectUrl.replace(/\/$/, '')}/${key}`
+
+      await Mail.send((message) => {
+        message.to(email)
+        message.from('contato@facebook.com', 'Facebook')
+        message.subject('Confirmação de cadastro')
+        message.htmlView('emails/register', { link })
+      })
+
+      return response.ok({ message: 'E-mail de ativação enviado com sucesso' })
     })
   }
 
-  public async show({}: HttpContextContract) {}
+  public async show({ params }: HttpContextContract) {
+    const userKey = await UserKey.findByOrFail('key', params.key)
 
-  public async update({}: HttpContextContract) {}
+    const user = await userKey.related('user').query().firstOrFail()
+
+    return user
+  }
+
+  public async update({ request, response }: HttpContextContract) {
+    const { key, name, password } = await request.validate(UpdateValidator)
+
+    const userKey = await UserKey.findByOrFail('key', key)
+
+    const user = await userKey.related('user').query().firstOrFail()
+
+    const username = name.split(' ')[0].toLowerCase() + new Date().getTime()
+
+    user.merge({ name, password, username })
+
+    await user.save()
+
+    await userKey.delete()
+
+    return response.ok({ message: 'Usuário atualizado com sucesso' })
+  }
 }
