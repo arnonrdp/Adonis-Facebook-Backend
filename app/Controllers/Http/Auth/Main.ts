5ce20f7dafd3be688421e0026d44f2630e8ace49
Arnon <arnonrdp@gmail.com>
2022-06-19 14:20:00 -0300

    Correção de response fail: de 400 para 401

--- app/Controllers/Http/Auth/Main.ts
+++ app/Controllers/Http/Auth/Main.ts
@@ -2,12 +2,15 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { StoreValidator } from 'App/Validators/Auth'
 
 export default class AuthController {
-  public async store({ request, auth }: HttpContextContract) {
+  public async store({ request, response, auth }: HttpContextContract) {
     const { email, password } = await request.validate(StoreValidator)
 
-    const token = await auth.attempt(email, password, { expiresIn: '30 days' })
-
-    return token
+    try {
+      const token = await auth.attempt(email, password, { expiresIn: '30 days' })
+      return token
+    } catch {
+      return response.unauthorized('Invalid credentials')
+    }
   }
 
   public async destroy({ auth }: HttpContextContract) {
